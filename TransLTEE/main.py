980d223159b477f96a28c3ff20f9388292264ef8
JiuChen0 <jiuchen1024@gmail.com>
2023-06-22 19:40:13 -0700

    basic config structure

--- TransLTEE/main.py
+++ TransLTEE/main.py
@@ -1,1 +1,16 @@
 #作为项目的入口，调用上述各个文件中的函数来完成项目的整个流程
+class Config:
+    """
+    模型和训练过程中的配置
+    """
+    def __init__(self):
+        # 训练参数
+        self.epochs = 100  # 训练轮次
+        self.batch_size = 64  # 批次大小
+        self.learning_rate = 0.001  # 学习率
+        self.weight_decay = 0.0001  # 权重衰减
+        self.save_dir = './home'  # 模型保存路径
+
+        # 模型参数
+        self.input_dim = 128  # 输入维度
+        self.hidden_dim = 256  # 隐藏层维度
