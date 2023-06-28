21b51eab8d8b03d0b0625fd0e3cf794fa7441c18
JiuChen0 <jiuchen1024@gmail.com>
2023-06-27 17:28:42 -0700

    change to tensorflow

--- TransLTEE/losses.py
+++ TransLTEE/losses.py
@@ -1,56 +1,47 @@
-import torch
-import torch.nn.functional as F
+import tensorflow as tf
 
 def compute_short_term_loss(surr_rep, encoded):
     # TODO: Implement the calculation of short-term loss
     # MSE Loss
-    loss = F.mse_loss(surr_rep, encoded)
+    loss = tf.keras.losses.MeanSquaredError()(surr_rep, encoded)
     return loss
 
 def compute_primary_outcome_loss(surr_rep, targets):
     # TODO: Implement the calculation of primary outcome loss
     # Cross Entropy Loss
-    loss = F.cross_entropy(surr_rep, targets)
+    loss = tf.keras.losses.CategoricalCrossentropy()(surr_rep, targets)
     return loss
 
 def compute_IPM_loss(encoded0, encoded1):
     # TODO: Implement the calculation of IPM loss (Wasserstein-1 distance)
     # Here we use a placeholder
-    loss = torch.tensor(0.0)
+    loss = tf.constant(0.0)
     return loss
 
-def compute_train_loss(model, batch, weights):
+def compute_train_loss(model, inputs, targets, weights):
     """
     Compute training loss
     model: The model object
     batch: A batch of training data
     weights: Weights for the three losses
     """
-    inputs, targets = batch
-    surr_rep, encoded0, encoded1 = model(inputs)
+    surr_rep, encoded0, encoded1 = model(inputs, training=True)
     loss1 = compute_short_term_loss(surr_rep, encoded0)
     loss2 = compute_primary_outcome_loss(surr_rep, targets)
     loss3 = compute_IPM_loss(encoded0, encoded1)
     loss = weights[0] * loss1 + weights[1] * loss2 + weights[2] * loss3
     return loss
 
-def compute_valid_loss(model, batch, weights):
+def compute_valid_loss(model, inputs, targets, weights):
     """
-    Compute validation loss
     Compute validation loss
     model: The model object
     batch: A batch of validation data
     weights: Weights for the three losses
     """
-    inputs, targets = batch
-    surr_rep, encoded0, encoded1 = model(inputs)
+    surr_rep, encoded0, encoded1 = model(inputs, training=False)
     loss1 = compute_short_term_loss(surr_rep, encoded0)
     loss2 = compute_primary_outcome_loss(surr_rep, targets)
     loss3 = compute_IPM_loss(encoded0, encoded1)
     loss = weights[0] * loss1 + weights[1] * loss2 + weights[2] * loss3
     return loss
-
-######################
-# If the model returns multiple outputs, or if the loss function requires additional parameters, 
-# you might need to make corresponding adjustments in these functions
-######################
