a50bf77a9d81411a0b9a66ece46934eb08bf4e8b
Austin Zhang <60564361+Austin6868@users.noreply.github.com>
2023-07-01 13:43:28 -0700

    fix: Modify data loading technique

--- TransLTEE/main.py
+++ TransLTEE/main.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from models import MyModel  # Please replace with your actual model
 from config import Config
+import numpy as np
 from data import get_dataloader
 import logging
 
@@ -27,11 +28,31 @@ def main():
     model = MyModel(config.input_dim, config.hidden_dim)
     logger.info('MODELS SUCCESSFULLY BUILT!')
 
+    # Load and process the data
+    t0=10
+
+    for j in range(1, 11):
+
+        TY = np.loadtxt('data/IHDP/csv/ihdp_npci_' + str(j) + '.csv', delimiter=',')
+        matrix = TY[:, 5:]
+        N = TY.shape[0]
+
+        out_treat = np.loadtxt('data/IHDP/Series_y_' + str(j) + '.txt', delimiter=',')
+        ts = out_treat[:, 0]
+        ts = np.reshape(ts, (N, 1))
+        ys = np.concatenate((out_treat[:, 1:(t0 + 1)], out_treat[:, -1].reshape(N, 1)), axis=1)
+        from sklearn.model_selection import train_test_split
+
+        matrix_rep = np.repeat(matrix[:, np.newaxis, :], t0, axis=1)
+        X_train, X_test, y_train, y_test, t_train, t_test = train_test_split(matrix_rep, ys, ts, test_size=0.2)
+
+        print(np.shape(X_train), np.shape(X_test), np.shape(y_train), np.shape(y_test), np.shape(t_train), np.shape(t_test))
+
     # Create the data loaders
-    logger.info('CREATING DATA LOADERS...')
-    train_dataloader = get_dataloader('train.csv', config.batch_size)
-    valid_dataloader = get_dataloader('valid.csv', config.batch_size)  # If there is a validation set
-    logger.info('DATA LOADERS SUCCESSFULLY CREATED!')
+    # logger.info('CREATING DATA LOADERS...')
+    # train_dataloader = get_dataloader('train.csv', config.batch_size)
+    # valid_dataloader = get_dataloader('valid.csv', config.batch_size)  # If there is a validation set
+    # logger.info('DATA LOADERS SUCCESSFULLY CREATED!')
 
     # Compile the model with the optimizer and loss function
     logger.info('COMPILING MODEL WITH THE OPTIMIZER AND LOSS FUNCTION')
