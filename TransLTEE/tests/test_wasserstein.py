de129b97dc4243558ba8328a8e5175ed2e747cf7
Austin Zhang <60564361+Austin6868@users.noreply.github.com>
2023-07-01 12:02:51 -0700

    feat: Implemented Wasserstein IPM loss and its unit tests

---
+++ TransLTEE/tests/test_wasserstein.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np
+from ..utils import wasserstein
+import tensorflow as tf
+
+@pytest.mark.parametrize("seq_len, lam, its, sq, backpropT", [
+    (3, 10, 10, False, False),
+    (4, 20, 20, True, True),
+    # add more parameter sets here as needed
+])
+def test_wasserstein(seq_len, lam, its, sq, backpropT):
+    # Create dummy inputs
+    X_ls = np.random.rand(10, seq_len, 5).astype(np.float32)
+    t = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
+    p = 0.5
+
+    # Call the function
+    result = wasserstein(seq_len, X_ls, t, p, lam, its, sq, backpropT)
+
+    # Add your assertions here.
+    assert isinstance(result, tf.Tensor)
+    assert result.dtype == tf.float32
