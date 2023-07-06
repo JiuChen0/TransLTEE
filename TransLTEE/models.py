1c6fae8acc891036e3b7e43416f553a82a2dae1b
JiuChen0 <jiuchen1024@gmail.com>
2023-07-05 18:15:57 -0700

    new branch

--- TransLTEE/models.py
+++ TransLTEE/models.py
@@ -56,34 +56,6 @@ class TransformerEncoder(tf.keras.layers.Layer):
 
         return x  # (batch_size, input_seq_len, d_model)
 
-# class DoubleHeadRNN(Model):
-#     def __init__(self, input_dim, hidden_dim):
-#         super(DoubleHeadRNN, self).__init__()
-#         self.rnn0 = RNN(GRUCell(hidden_dim))  # GRU is used here, replace with LSTMCell for LSTM
-#         self.rnn1 = RNN(GRUCell(hidden_dim))
-
-#     def call(self, x):
-#         output0 = self.rnn0(x)
-#         output1 = self.rnn1(x)
-#         return output0, output1
-
-# class TransformerEncoder(Model):
-#     def __init__(self):
-#         super(TransformerEncoder, self).__init__()
-#         self.transformer = TFBertModel.from_pretrained('bert-base-uncased')
-
-#     def call(self, x):
-#         outputs = self.transformer(x)
-#         return outputs.last_hidden_state
-
-# class TransformerDecoder(Model):
-#     def __init__(self):
-#         super(TransformerDecoder, self).__init__()
-#         self.transformer = TFBertForMaskedLM.from_pretrained('bert-base-uncased')
-
-#     def call(self, x):
-#         outputs = self.transformer(x)
-#         return outputs.logits
 
 class MyModel(Model):
     def __init__(self, input_dim, num_layers=7, num_heads=5, dff=50, dropout_rate=0.1):
